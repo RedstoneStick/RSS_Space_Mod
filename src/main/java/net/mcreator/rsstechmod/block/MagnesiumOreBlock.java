c940c6b4b652e0a90f20b8514a6a089f903dbff0
RedstoneStick <onatkut@DESKTOP-RODSACK>
2022-03-18 20:48:55 +0300

    - Made Magnesium Ore Flammable

--- src/main/java/net/mcreator/rsstechmod/block/MagnesiumOreBlock.java
+++ src/main/java/net/mcreator/rsstechmod/block/MagnesiumOreBlock.java
@@ -7,8 +7,6 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.api.distmarker.Dist;
 
 import net.minecraft.world.gen.feature.template.RuleTest;
 import net.minecraft.world.gen.feature.template.IRuleTestType;
@@ -26,13 +24,10 @@ import net.minecraft.util.registry.Registry;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.RegistryKey;
-import net.minecraft.particles.ParticleTypes;
 import net.minecraft.loot.LootContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Item;
 import net.minecraft.item.BlockItem;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.client.Minecraft;
 import net.minecraft.block.material.MaterialColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.SoundType;
@@ -40,12 +35,17 @@ import net.minecraft.block.Blocks;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Block;
 
+import net.mcreator.rsstechmod.procedures.MagnesOreIgnitionProcedure;
 import net.mcreator.rsstechmod.itemgroup.OresCreativeTabItemGroup;
 import net.mcreator.rsstechmod.RssTechModModElements;
 
+import java.util.stream.Stream;
 import java.util.Random;
+import java.util.Map;
 import java.util.List;
+import java.util.HashMap;
 import java.util.Collections;
+import java.util.AbstractMap;
 
 @RssTechModModElements.ModElement.Tag
 public class MagnesiumOreBlock extends RssTechModModElements.ModElement {
@@ -89,25 +89,20 @@ public class MagnesiumOreBlock extends RssTechModModElements.ModElement {
 			return Collections.singletonList(new ItemStack(this, 1));
 		}
 
-		@OnlyIn(Dist.CLIENT)
 		@Override
-		public void animateTick(BlockState blockstate, World world, BlockPos pos, Random random) {
-			super.animateTick(blockstate, world, pos, random);
-			PlayerEntity entity = Minecraft.getInstance().player;
+		public void neighborChanged(BlockState blockstate, World world, BlockPos pos, Block neighborBlock, BlockPos fromPos, boolean moving) {
+			super.neighborChanged(blockstate, world, pos, neighborBlock, fromPos, moving);
 			int x = pos.getX();
 			int y = pos.getY();
 			int z = pos.getZ();
-			if (true)
-				for (int l = 0; l < 4; ++l) {
-					double d0 = (x + random.nextFloat());
-					double d1 = (y + random.nextFloat());
-					double d2 = (z + random.nextFloat());
-					int i1 = random.nextInt(2) * 2 - 1;
-					double d3 = (random.nextFloat() - 0.5D) * 0.5D;
-					double d4 = (random.nextFloat() - 0.5D) * 0.5D;
-					double d5 = (random.nextFloat() - 0.5D) * 0.5D;
-					world.addParticle(ParticleTypes.LAVA, d0, d1, d2, d3, d4, d5);
-				}
+			if (world.getRedstonePowerFromNeighbors(new BlockPos(x, y, z)) > 0) {
+			} else {
+			}
+
+			MagnesOreIgnitionProcedure.executeProcedure(Stream
+					.of(new AbstractMap.SimpleEntry<>("world", world), new AbstractMap.SimpleEntry<>("x", x), new AbstractMap.SimpleEntry<>("y", y),
+							new AbstractMap.SimpleEntry<>("z", z))
+					.collect(HashMap::new, (_m, _e) -> _m.put(_e.getKey(), _e.getValue()), Map::putAll));
 		}
 	}
 
