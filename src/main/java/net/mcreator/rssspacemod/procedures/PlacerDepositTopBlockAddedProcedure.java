0a297a0ce6e7364312e7b9c149e8c2ac046e1aab
RedstoneStick <acer@DESKTOP-8F3O5KQ>
2022-04-03 16:28:22 +0300

    -placer deposits 1

---
+++ src/main/java/net/mcreator/rssspacemod/procedures/PlacerDepositTopBlockAddedProcedure.java
@@ -0,0 +1,114 @@
+package net.mcreator.rssspacemod.procedures;
+
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.rssspacemod.block.OreBlackSandUraniumBlock;
+import net.mcreator.rssspacemod.block.OreBlackSandTitaniumBlock;
+import net.mcreator.rssspacemod.block.OreBlackSandThoriumBlock;
+import net.mcreator.rssspacemod.block.OreBlackSandPlatinumBlock;
+import net.mcreator.rssspacemod.block.OreBlackSandGoldBlock;
+import net.mcreator.rssspacemod.block.OreBlackSandDiamondBlock;
+import net.mcreator.rssspacemod.block.OreBlackSandBlock;
+import net.mcreator.rssspacemod.RssSpaceModMod;
+
+import java.util.Map;
+
+public class PlacerDepositTopBlockAddedProcedure {
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				RssSpaceModMod.LOGGER.warn("Failed to load dependency world for procedure PlacerDepositTopBlockAdded!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				RssSpaceModMod.LOGGER.warn("Failed to load dependency x for procedure PlacerDepositTopBlockAdded!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				RssSpaceModMod.LOGGER.warn("Failed to load dependency y for procedure PlacerDepositTopBlockAdded!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				RssSpaceModMod.LOGGER.warn("Failed to load dependency z for procedure PlacerDepositTopBlockAdded!");
+			return;
+		}
+		IWorld world = (IWorld) dependencies.get("world");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		if (!world.isRemote()) {
+			BlockPos _bp = new BlockPos((int) x, (int) y, (int) z);
+			TileEntity _tileEntity = world.getTileEntity(_bp);
+			BlockState _bs = world.getBlockState(_bp);
+			if (_tileEntity != null)
+				_tileEntity.getTileData().putDouble("Num", Math.random());
+			if (world instanceof World)
+				((World) world).notifyBlockUpdate(_bp, _bs, _bs, 3);
+		}
+		if (0.1 >= new Object() {
+			public double getValue(IWorld world, BlockPos pos, String tag) {
+				TileEntity tileEntity = world.getTileEntity(pos);
+				if (tileEntity != null)
+					return tileEntity.getTileData().getDouble(tag);
+				return -1;
+			}
+		}.getValue(world, new BlockPos((int) x, (int) y, (int) z), "Num")) {
+			world.setBlockState(new BlockPos((int) x, (int) y, (int) z), OreBlackSandDiamondBlock.block.getDefaultState(), 3);
+		} else if (0.3 >= new Object() {
+			public double getValue(IWorld world, BlockPos pos, String tag) {
+				TileEntity tileEntity = world.getTileEntity(pos);
+				if (tileEntity != null)
+					return tileEntity.getTileData().getDouble(tag);
+				return -1;
+			}
+		}.getValue(world, new BlockPos((int) x, (int) y, (int) z), "Num")) {
+			world.setBlockState(new BlockPos((int) x, (int) y, (int) z), OreBlackSandTitaniumBlock.block.getDefaultState(), 3);
+		} else if (0.4 >= new Object() {
+			public double getValue(IWorld world, BlockPos pos, String tag) {
+				TileEntity tileEntity = world.getTileEntity(pos);
+				if (tileEntity != null)
+					return tileEntity.getTileData().getDouble(tag);
+				return -1;
+			}
+		}.getValue(world, new BlockPos((int) x, (int) y, (int) z), "Num")) {
+			world.setBlockState(new BlockPos((int) x, (int) y, (int) z), OreBlackSandThoriumBlock.block.getDefaultState(), 3);
+		} else if (0.45 >= new Object() {
+			public double getValue(IWorld world, BlockPos pos, String tag) {
+				TileEntity tileEntity = world.getTileEntity(pos);
+				if (tileEntity != null)
+					return tileEntity.getTileData().getDouble(tag);
+				return -1;
+			}
+		}.getValue(world, new BlockPos((int) x, (int) y, (int) z), "Num")) {
+			world.setBlockState(new BlockPos((int) x, (int) y, (int) z), OreBlackSandUraniumBlock.block.getDefaultState(), 3);
+		} else if (0.5 >= new Object() {
+			public double getValue(IWorld world, BlockPos pos, String tag) {
+				TileEntity tileEntity = world.getTileEntity(pos);
+				if (tileEntity != null)
+					return tileEntity.getTileData().getDouble(tag);
+				return -1;
+			}
+		}.getValue(world, new BlockPos((int) x, (int) y, (int) z), "Num")) {
+			world.setBlockState(new BlockPos((int) x, (int) y, (int) z), OreBlackSandGoldBlock.block.getDefaultState(), 3);
+		} else if (0.55 >= new Object() {
+			public double getValue(IWorld world, BlockPos pos, String tag) {
+				TileEntity tileEntity = world.getTileEntity(pos);
+				if (tileEntity != null)
+					return tileEntity.getTileData().getDouble(tag);
+				return -1;
+			}
+		}.getValue(world, new BlockPos((int) x, (int) y, (int) z), "Num")) {
+			world.setBlockState(new BlockPos((int) x, (int) y, (int) z), OreBlackSandPlatinumBlock.block.getDefaultState(), 3);
+		} else {
+			world.setBlockState(new BlockPos((int) x, (int) y, (int) z), OreBlackSandBlock.block.getDefaultState(), 3);
+		}
+	}
+}
