381f2e4c89fb99adcc961ef71d8759f2c6c1ae01
RedstoneStick <onatkut@DESKTOP-RODSACK>
2022-05-13 19:42:13 +0300

    ~Crashed Sattelite Model

--- models/Modelcrashed_sattelite.java
+++ models/Modelcrashed_sattelite.java
@@ -1,10 +1,28 @@
-// Made with Blockbench 4.2.2
+// Made with Blockbench 4.2.4
 // Exported for Minecraft version 1.15 - 1.16 with MCP mappings
 // Paste this class into your mod and generate all required imports
 
 public static class Modelcrashed_sattelite extends EntityModel<Entity> {
 	private final ModelRenderer bone;
+	private final ModelRenderer cube_r1;
 	private final ModelRenderer cube3_r1;
+	private final ModelRenderer thruster1;
+	private final ModelRenderer cube_r2;
+	private final ModelRenderer cube_r3;
+	private final ModelRenderer cube_r4;
+	private final ModelRenderer cube_r5;
+	private final ModelRenderer thruster2;
+	private final ModelRenderer cube_r6;
+	private final ModelRenderer cube_r7;
+	private final ModelRenderer cube_r8;
+	private final ModelRenderer cube_r9;
+	private final ModelRenderer thruster3;
+	private final ModelRenderer cube_r10;
+	private final ModelRenderer cube_r11;
+	private final ModelRenderer cube_r12;
+	private final ModelRenderer cube_r13;
+	private final ModelRenderer decor;
+	private final ModelRenderer solar_r1;
 
 	public Modelcrashed_sattelite() {
 		textureWidth = 128;
@@ -13,31 +31,135 @@ public static class Modelcrashed_sattelite extends EntityModel<Entity> {
 		bone = new ModelRenderer(this);
 		bone.setRotationPoint(-16.0F, 14.0F, 0.0F);
 		setRotationAngle(bone, 0.0F, 0.0F, 1.8326F);
-		bone.setTextureOffset(0, 0).addBox(-6.0F, -32.0F, -6.0F, 12.0F, 32.0F, 12.0F, 0.0F, false);
-		bone.setTextureOffset(32, 28).addBox(-8.0F, 0.0F, -8.0F, 16.0F, 2.0F, 16.0F, 0.0F, false);
+		bone.setTextureOffset(36, 32).addBox(-6.0F, -32.0F, -6.0F, 12.0F, 32.0F, 12.0F, 0.0F, false);
+		bone.setTextureOffset(48, 0).addBox(-8.0F, -2.0F, -8.0F, 16.0F, 4.0F, 16.0F, 0.0F, false);
+		bone.setTextureOffset(70, 62).addBox(-7.0F, -3.0F, -7.0F, 14.0F, 2.0F, 14.0F, -0.4F, false);
+		bone.setTextureOffset(34, 76).addBox(-9.0F, 0.0F, -7.0F, 1.0F, 8.0F, 14.0F, 0.0F, false);
+		bone.setTextureOffset(72, 20).addBox(8.0F, 0.0F, -7.0F, 1.0F, 8.0F, 14.0F, 0.0F, false);
+		bone.setTextureOffset(48, 20).addBox(-7.0F, 1.0F, 8.0F, 14.0F, 7.0F, 1.0F, 0.0F, false);
+		bone.setTextureOffset(0, 44).addBox(-7.0F, 1.0F, -9.0F, 14.0F, 7.0F, 1.0F, 0.0F, false);
+
+		cube_r1 = new ModelRenderer(this);
+		cube_r1.setRotationPoint(0.0F, 0.0F, 0.0F);
+		bone.addChild(cube_r1);
+		setRotationAngle(cube_r1, 0.0F, -0.7854F, 0.0F);
+		cube_r1.setTextureOffset(0, 0).addBox(-6.0F, -32.0F, -6.0F, 12.0F, 32.0F, 12.0F, 0.01F, false);
 
 		cube3_r1 = new ModelRenderer(this);
-		cube3_r1.setRotationPoint(1.2426F, -33.2574F, -1.6213F);
+		cube3_r1.setRotationPoint(0.2426F, -34.2574F, -2.6213F);
 		bone.addChild(cube3_r1);
-		setRotationAngle(cube3_r1, -0.7854F, 0.0F, 0.7854F);
-		cube3_r1.setTextureOffset(0, 44).addBox(-5.0F, -5.0F, -3.0F, 10.0F, 10.0F, 10.0F, -0.1F, false);
-	}
+		setRotationAngle(cube3_r1, -0.7854F, 0.0F, -0.7854F);
+		cube3_r1.setTextureOffset(0, 64).addBox(-7.0F, -7.0F, -3.0F, 12.0F, 12.0F, 12.0F, 0.0F, false);
 
-	@Override
-	public void setRotationAngles(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks,
-			float netHeadYaw, float headPitch) {
-		// previously the render function, render code was moved to a method below
+		thruster1 = new ModelRenderer(this);
+		thruster1.setRotationPoint(0.0F, -2.0F, 0.0F);
+		bone.addChild(thruster1);
+
+		cube_r2 = new ModelRenderer(this);
+		cube_r2.setRotationPoint(0.0F, 0.0F, -2.0F);
+		thruster1.addChild(cube_r2);
+		setRotationAngle(cube_r2, 0.0F, 0.0F, -0.2182F);
+		cube_r2.setTextureOffset(96, 0).addBox(-4.0F, 2.0F, 3.0F, 1.0F, 9.0F, 6.0F, 0.0F, false);
+
+		cube_r3 = new ModelRenderer(this);
+		cube_r3.setRotationPoint(0.0F, 0.0F, 2.0F);
+		thruster1.addChild(cube_r3);
+		setRotationAngle(cube_r3, 0.0F, -1.5708F, 0.0436F);
+		cube_r3.setTextureOffset(72, 93).addBox(-1.1F, 4.0F, 1.0F, 1.0F, 9.0F, 6.0F, 0.0F, false);
+
+		cube_r4 = new ModelRenderer(this);
+		cube_r4.setRotationPoint(0.0F, 0.0F, 0.0F);
+		thruster1.addChild(cube_r4);
+		setRotationAngle(cube_r4, 0.0F, -1.5708F, 0.0436F);
+		cube_r4.setTextureOffset(94, 78).addBox(6.1F, 4.0F, 1.0F, 1.0F, 9.0F, 6.0F, 0.0F, false);
+
+		cube_r5 = new ModelRenderer(this);
+		cube_r5.setRotationPoint(0.0F, 0.0F, -2.0F);
+		thruster1.addChild(cube_r5);
+		setRotationAngle(cube_r5, 0.0F, 0.0F, 0.2618F);
+		cube_r5.setTextureOffset(94, 96).addBox(-5.0F, 4.0F, 3.0F, 1.0F, 9.0F, 6.0F, 0.0F, false);
+
+		thruster2 = new ModelRenderer(this);
+		thruster2.setRotationPoint(0.0F, -2.0F, -10.0F);
+		bone.addChild(thruster2);
+
+		cube_r6 = new ModelRenderer(this);
+		cube_r6.setRotationPoint(0.0F, 0.0F, 0.0F);
+		thruster2.addChild(cube_r6);
+		setRotationAngle(cube_r6, 0.0F, 0.0F, -0.2182F);
+		cube_r6.setTextureOffset(96, 0).addBox(-4.0F, 2.0F, 3.0F, 1.0F, 9.0F, 6.0F, 0.0F, false);
+
+		cube_r7 = new ModelRenderer(this);
+		cube_r7.setRotationPoint(0.0F, 0.0F, 4.0F);
+		thruster2.addChild(cube_r7);
+		setRotationAngle(cube_r7, 0.0F, -1.5708F, 0.0436F);
+		cube_r7.setTextureOffset(72, 93).addBox(-1.1F, 4.0F, 1.0F, 1.0F, 9.0F, 6.0F, 0.0F, false);
+
+		cube_r8 = new ModelRenderer(this);
+		cube_r8.setRotationPoint(0.0F, 0.0F, 0.0F);
+		thruster2.addChild(cube_r8);
+		setRotationAngle(cube_r8, 0.0F, -1.5708F, 0.0436F);
+		cube_r8.setTextureOffset(94, 78).addBox(8.1F, 4.0F, 1.0F, 1.0F, 9.0F, 6.0F, 0.0F, false);
+
+		cube_r9 = new ModelRenderer(this);
+		cube_r9.setRotationPoint(0.0F, 0.0F, 0.0F);
+		thruster2.addChild(cube_r9);
+		setRotationAngle(cube_r9, 0.0F, 0.0F, 0.2618F);
+		cube_r9.setTextureOffset(94, 96).addBox(-5.0F, 4.0F, 3.0F, 1.0F, 9.0F, 6.0F, 0.0F, false);
+
+		thruster3 = new ModelRenderer(this);
+		thruster3.setRotationPoint(8.0F, -2.0F, -6.0F);
+		bone.addChild(thruster3);
+
+		cube_r10 = new ModelRenderer(this);
+		cube_r10.setRotationPoint(0.0F, 0.0F, 0.0F);
+		thruster3.addChild(cube_r10);
+		setRotationAngle(cube_r10, 0.0F, 0.0F, -0.2182F);
+		cube_r10.setTextureOffset(96, 0).addBox(-4.0F, 2.0F, 3.0F, 1.0F, 9.0F, 6.0F, 0.0F, false);
+
+		cube_r11 = new ModelRenderer(this);
+		cube_r11.setRotationPoint(0.0F, 0.0F, 4.0F);
+		thruster3.addChild(cube_r11);
+		setRotationAngle(cube_r11, 0.0F, -1.5708F, 0.0436F);
+		cube_r11.setTextureOffset(72, 93).addBox(-1.1F, 4.0F, 1.0F, 1.0F, 9.0F, 6.0F, 0.0F, false);
+
+		cube_r12 = new ModelRenderer(this);
+		cube_r12.setRotationPoint(0.0F, 0.0F, 0.0F);
+		thruster3.addChild(cube_r12);
+		setRotationAngle(cube_r12, 0.0F, -1.5708F, 0.0436F);
+		cube_r12.setTextureOffset(94, 78).addBox(8.1F, 4.0F, 1.0F, 1.0F, 9.0F, 6.0F, 0.0F, false);
+
+		cube_r13 = new ModelRenderer(this);
+		cube_r13.setRotationPoint(0.0F, 0.0F, 0.0F);
+		thruster3.addChild(cube_r13);
+		setRotationAngle(cube_r13, 0.0F, 0.0F, 0.2618F);
+		cube_r13.setTextureOffset(94, 96).addBox(-5.0F, 4.0F, 3.0F, 1.0F, 9.0F, 6.0F, 0.0F, false);
+
+		decor = new ModelRenderer(this);
+		decor.setRotationPoint(0.0F, 0.0F, 0.0F);
+		bone.addChild(decor);
+		decor.setTextureOffset(0, 89).addBox(-2.0F, -28.0F, 5.0F, 8.0F, 9.0F, 1.0F, -0.1F, false);
+		decor.setTextureOffset(0, 102).addBox(-3.0F, -11.0F, 5.0F, 6.0F, 6.0F, 2.0F, 0.0F, false);
+
+		solar_r1 = new ModelRenderer(this);
+		solar_r1.setRotationPoint(0.0F, -16.0F, 0.0F);
+		decor.addChild(solar_r1);
+		setRotationAngle(solar_r1, 0.0F, -0.7854F, 0.0F);
+		solar_r1.setTextureOffset(0, 116).addBox(-28.0F, -5.0F, 0.0F, 40.0F, 11.0F, 0.0F, 0.1F, false);
 	}
 
 	@Override
 	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red,
 			float green, float blue, float alpha) {
-		bone.render(matrixStack, buffer, packedLight, packedOverlay);
+		bone.render(matrixStack, buffer, packedLight, packedOverlay, red, green, blue, alpha);
 	}
 
 	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
 		modelRenderer.rotateAngleX = x;
 		modelRenderer.rotateAngleY = y;
 		modelRenderer.rotateAngleZ = z;
+	}
+
+	public void setRotationAngles(float f, float f1, float f2, float f3, float f4, float f5, Entity e) {
 	}
 }
--- src/main/java/net/mcreator/rssspacemod/entity/CrashedSatteliteEntity.java
+++ src/main/java/net/mcreator/rssspacemod/entity/CrashedSatteliteEntity.java
@@ -12,12 +12,13 @@ import net.minecraftforge.common.ForgeMod;
 
 import net.minecraft.world.World;
 import net.minecraft.world.IWorldReader;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.Hand;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.ActionResultType;
-import net.minecraft.pathfinding.FlyingPathNavigator;
+import net.minecraft.pathfinding.SwimmerPathNavigator;
+import net.minecraft.pathfinding.PathNodeType;
 import net.minecraft.network.IPacket;
 import net.minecraft.item.SpawnEggItem;
 import net.minecraft.item.ItemStack;
@@ -25,7 +26,7 @@ import net.minecraft.item.Item;
 import net.minecraft.entity.projectile.PotionEntity;
 import net.minecraft.entity.projectile.AbstractArrowEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.ai.controller.FlyingMovementController;
+import net.minecraft.entity.ai.controller.MovementController;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.MobEntity;
@@ -34,7 +35,6 @@ import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.CreatureEntity;
 import net.minecraft.entity.CreatureAttribute;
-import net.minecraft.block.BlockState;
 
 import net.mcreator.rssspacemod.procedures.GCRocketRightClickedOnEntityProcedure;
 import net.mcreator.rssspacemod.itemgroup.DevToolsCreativeTabItemGroup;
@@ -73,12 +73,11 @@ public class CrashedSatteliteEntity extends RssSpaceModModElements.ModElement {
 		@SubscribeEvent
 		public void onEntityAttributeCreation(EntityAttributeCreationEvent event) {
 			AttributeModifierMap.MutableAttribute ammma = MobEntity.func_233666_p_();
-			ammma = ammma.createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.3);
+			ammma = ammma.createMutableAttribute(Attributes.MOVEMENT_SPEED, 0);
 			ammma = ammma.createMutableAttribute(Attributes.MAX_HEALTH, 6);
-			ammma = ammma.createMutableAttribute(Attributes.ARMOR, 6);
-			ammma = ammma.createMutableAttribute(Attributes.ATTACK_DAMAGE, 3);
-			ammma = ammma.createMutableAttribute(Attributes.FLYING_SPEED, 0.3);
-			ammma = ammma.createMutableAttribute(ForgeMod.SWIM_SPEED.get(), 0.3);
+			ammma = ammma.createMutableAttribute(Attributes.ARMOR, 0);
+			ammma = ammma.createMutableAttribute(Attributes.ATTACK_DAMAGE, 0);
+			ammma = ammma.createMutableAttribute(ForgeMod.SWIM_SPEED.get(), 0);
 			event.put(entity, ammma.create());
 		}
 	}
@@ -93,8 +92,40 @@ public class CrashedSatteliteEntity extends RssSpaceModModElements.ModElement {
 			experienceValue = 0;
 			setNoAI(true);
 			enablePersistence();
-			this.moveController = new FlyingMovementController(this, 10, true);
-			this.navigator = new FlyingPathNavigator(this, this.world);
+			this.setPathPriority(PathNodeType.WATER, 0);
+			this.moveController = new MovementController(this) {
+				@Override
+				public void tick() {
+					if (CustomEntity.this.isInWater())
+						CustomEntity.this.setMotion(CustomEntity.this.getMotion().add(0, 0.005, 0));
+					if (this.action == MovementController.Action.MOVE_TO && !CustomEntity.this.getNavigator().noPath()) {
+						double dx = this.posX - CustomEntity.this.getPosX();
+						double dy = this.posY - CustomEntity.this.getPosY();
+						double dz = this.posZ - CustomEntity.this.getPosZ();
+						float f = (float) (MathHelper.atan2(dz, dx) * (double) (180 / Math.PI)) - 90;
+						float f1 = (float) (this.speed * CustomEntity.this.getAttribute(Attributes.MOVEMENT_SPEED).getValue());
+						CustomEntity.this.rotationYaw = this.limitAngle(CustomEntity.this.rotationYaw, f, 10);
+						CustomEntity.this.renderYawOffset = CustomEntity.this.rotationYaw;
+						CustomEntity.this.rotationYawHead = CustomEntity.this.rotationYaw;
+						if (CustomEntity.this.isInWater()) {
+							CustomEntity.this.setAIMoveSpeed((float) CustomEntity.this.getAttribute(Attributes.MOVEMENT_SPEED).getValue());
+							float f2 = -(float) (MathHelper.atan2(dy, MathHelper.sqrt(dx * dx + dz * dz)) * (180F / Math.PI));
+							f2 = MathHelper.clamp(MathHelper.wrapDegrees(f2), -85, 85);
+							CustomEntity.this.rotationPitch = this.limitAngle(CustomEntity.this.rotationPitch, f2, 5);
+							float f3 = MathHelper.cos(CustomEntity.this.rotationPitch * (float) (Math.PI / 180.0));
+							CustomEntity.this.setMoveForward(f3 * f1);
+							CustomEntity.this.setMoveVertical((float) (f1 * dy));
+						} else {
+							CustomEntity.this.setAIMoveSpeed(f1 * 0.05F);
+						}
+					} else {
+						CustomEntity.this.setAIMoveSpeed(0);
+						CustomEntity.this.setMoveVertical(0);
+						CustomEntity.this.setMoveForward(0);
+					}
+				}
+			};
+			this.navigator = new SwimmerPathNavigator(this, this.world);
 		}
 
 		@Override
@@ -123,11 +154,6 @@ public class CrashedSatteliteEntity extends RssSpaceModModElements.ModElement {
 		}
 
 		@Override
-		public boolean onLivingFall(float l, float d) {
-			return false;
-		}
-
-		@Override
 		public boolean attackEntityFrom(DamageSource source, float amount) {
 			if (source.getImmediateSource() instanceof AbstractArrowEntity)
 				return false;
@@ -141,11 +167,7 @@ public class CrashedSatteliteEntity extends RssSpaceModModElements.ModElement {
 				return false;
 			if (source == DamageSource.DROWN)
 				return false;
-			if (source == DamageSource.LIGHTNING_BOLT)
-				return false;
 			if (source.getDamageType().equals("trident"))
-				return false;
-			if (source == DamageSource.ANVIL)
 				return false;
 			return super.attackEntityFrom(source, amount);
 		}
@@ -176,20 +198,6 @@ public class CrashedSatteliteEntity extends RssSpaceModModElements.ModElement {
 		@Override
 		public boolean isPushedByWater() {
 			return false;
-		}
-
-		@Override
-		protected void updateFallState(double y, boolean onGroundIn, BlockState state, BlockPos pos) {
-		}
-
-		@Override
-		public void setNoGravity(boolean ignored) {
-			super.setNoGravity(true);
-		}
-
-		public void livingTick() {
-			super.livingTick();
-			this.setNoGravity(true);
 		}
 	}
 }
